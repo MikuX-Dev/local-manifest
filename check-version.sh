1b3ea8926ca8127b255a7537205be4b8a9dddea5
Alexandre Roux <alexandre.roux.danzi@lostpod.me>
2022-10-12 09:19:53 +0000

    Update check-version.sh for android S

--- check-version.sh
+++ check-version.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 VENDOR="android_vendor_lineage"
+VERSION_FILE="config/common.mk"
 
 if [[ $CI_COMMIT_TAG == *"nougat"* ]]
 then
   VENDOR="android_vendor_cm"
 fi
 
+if [[ $CI_COMMIT_TAG == *"s"* ]]
+then
+  VERSION_FILE="config/version.mk"
+fi
+
 VENDOR_COMMIT_ID=$(cat default.xml | grep $VENDOR | sed -E 's/.*revision="(\S*)" .*/\1/')
 
-curl -s https://gitlab.e.foundation/e/os/$VENDOR/-/raw/$VENDOR_COMMIT_ID/config/common.mk -o common.mk
+curl -s https://gitlab.e.foundation/e/os/$VENDOR/-/raw/$VENDOR_COMMIT_ID/$VERSION_FILE -o common.mk
 PRODUCT_VERSION_MAJOR=$(cat common.mk | grep "PRODUCT_VERSION_MAJOR = " | sed -E 's/PRODUCT_VERSION_MAJOR = ([0-9]*)/\1/')
 PRODUCT_VERSION_MINOR=$(cat common.mk | grep "PRODUCT_VERSION_MINOR = " | sed -E 's/PRODUCT_VERSION_MINOR = ([0-9]*)/\1/')
 PRODUCT_VERSION="$PRODUCT_VERSION_MAJOR.$PRODUCT_VERSION_MINOR"
